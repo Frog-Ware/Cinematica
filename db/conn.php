35936f67a6cc72f745b73f1320a1594a53809589
nnsfrtl <sugoferreiranahuel@gmail.com>
2024-06-11 19:18:16 -0300

    Add files via upload
    Añadidos los archivos PHP que permiten iniciar sesión, registrar un usuario nuevo en la BD y checkear si la sesion de un usuario esta iniciada.

---
+++ db/conn.php
@@ -0,0 +1,16 @@
+<?php
+
+// Este script conecta a la base de datos mediante PDO.
+
+$host = "localhost";
+$dbname = "Cinematica";
+$username = "root";
+$password = "";
+
+try {
+    $con = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+} catch (PDOException $pe) {
+    die ("Could not connect to the database $dbname :" . $pe->getMessage());
+}
+
+?>
---
+++ log/islogged.php
@@ -0,0 +1,16 @@
+<?php
+
+// Este script devuelve los datos del usuario de estar la sesión activa. Si no es así, devuelve un error.
+
+header("Content-Type: application/json");
+session_start();
+require ("../db/retrieve.php");
+
+// Si hay una sesión iniciada, envia los datos del usuario via JSON. Si no es así, devuelve un error.
+if(isset($_SESSION['user'])) {
+    echo json_encode(traerUsuario($_POST['email']));
+} else {
+    echo json_encode(['error' => "No esta iniciada la sesión."]);
+}
+
+?>

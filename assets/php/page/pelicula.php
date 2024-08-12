c2ec8d87b24aa4b5f402ba3a1ddd04f4b1bf0161
nnsfrtl <sugoferreiranahuel@gmail.com>
2024-08-12 19:08:58 -0300

    php update

--- assets/php/page/pelicula.php
+++ assets/php/page/pelicula.php
@@ -6,9 +6,13 @@ header("Content-Type: application/json; charset=utf-8");
 require_once "../db/traer.php";
 require_once "../config/acceso.php";
 
-// Asigna el valor de búsqueda a una variable.
-$var = "%" . filter_input(INPUT_POST, 'busqueda', FILTER_SANITIZE_STRING) . "%";
-$datos = traerBusqueda($var);
+// Trae los datos de la pelicula por ID o nombre, según lo enviado
+if (isset($_POST['idProducto']))
+    $datos = traerPelicula($_POST['idProducto']);
+else if (isset($_POST['nombrePelicula']))
+    $datos = traerPeliculaNombre($_POST['nombrePelicula']);
+else 
+    $datos = null;
 $response = ($datos != null) ?
     ['resultado' => $datos] : ['error' => 'No hay artículos disponibles'];
 
